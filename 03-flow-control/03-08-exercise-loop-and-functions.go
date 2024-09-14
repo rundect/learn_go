d9b96424b0fb8c7a986d98ea1a65f78cde4773b8
develop <dofastsite@gmail.com>
2024-09-14 03:51:29 +0300

    End 03-09

--- 03-flow-control/03-08-exercise-loop-and-functions.go
+++ 03-flow-control/03-08-exercise-loop-and-functions.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-	var a float64
+	var result float64
+	returning_result := 0.0
 	for z := 1.0; z < (x + 0.1); z += 0.1{
-		a = z - ((z*z - x) / (2*z))
+		result = z - ((z*z - x) / (2*z))
+		powed_result := math.Pow(result, 2)
+		if powed_result > returning_result && powed_result < x{
+			returning_result = result
+		}
 	}
-	return a
+	return returning_result
 }
 
 func main() {
