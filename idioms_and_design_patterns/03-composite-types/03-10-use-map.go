c4c2fc613d6dd8e2b51543e3a2a063c485c516e9
develop <dofastsite@gmail.com>
2024-09-26 20:23:51 +0300

    End 03-10

---
+++ idioms_and_design_patterns/03-composite-types/03-10-use-map.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	totalWins := map[string]int{}
+	totalWins["Orcas"] = 1
+	totalWins["Lions"] = 2
+	fmt.Println(totalWins["Orcas"])
+	fmt.Println(totalWins["Kittens"])
+	totalWins["Kittens"]++
+	fmt.Println(totalWins["Kittens"])
+	totalWins["Lions"] = 3
+	fmt.Println(totalWins["Lions"])
+}
