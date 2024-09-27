9ddea93363392faee77cf577eaa842f81852bc51
develop <dofastsite@gmail.com>
2024-09-27 19:32:05 +0300

    End 04-06

---
+++ idioms_and_design_patterns/04-blocks-variable-shading-and-control-structures/04-05-if-else.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	n := rand.Intn(10)
+	if n == 0 {
+	 fmt.Println("That's too low")
+	} else if n > 5 {
+	 fmt.Println("That's too big:", n)
+	} else {
+	 fmt.Println("That's a good number:", n)
+	}
+}
